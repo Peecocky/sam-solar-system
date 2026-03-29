236146725fb5f15642443cb5e194b271b104e794
Peecocky <ss19608@nyu.edu>
2026-03-29 23:58:00 +0800

    af

--- app/minecraft/page.tsx
+++ app/minecraft/page.tsx
@@ -22,11 +22,11 @@ const MINECRAFT_IMAGES = [
 ]
 
 const VIDEO_CLIPS = [
-  { src: '/minecraft loop video.mp4' },
-  { src: '/minecraft loop video.mp4' },
-  { src: '/minecraft loop video.mp4' },
-  { src: '/minecraft loop video.mp4' },
-  { src: '/minecraft loop video.mp4' },
+  { src: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' },
+  { src: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4' },
+  { src: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4' },
+  { src: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4' },
+  { src: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4' },
 ]
 
 export default function MinecraftPage() {
